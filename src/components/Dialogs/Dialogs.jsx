fbcd300fecea4304262825a67c85058034086cf5
Nikolai <pekarskiy.nikolai@gmail.com>
2019-07-09 09:44:17 +0300

    lesson 18 finished. Add new components Dialogs

---
+++ src/components/Dialogs/Dialogs.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import css from './Dialogs.module.css'; 
+
+const Dialogs = (props) => {
+    return (
+        <div className={css.dialogs_box}>
+            Dialogs
+        </div>
+    )
+}
+
+export default Dialogs;
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -3,7 +3,7 @@ import css from './profile.module.css';
 import MyPosts from './Posts/MyPosts/MyPosts';
 
 const Profile = () => {
-  return <div className={css.content}>
+  return <div>
     <div className={css.profile__img_box}>
       <img className={css.profile__img} src="http://bishop.usssulaco.ru/_bl/1/50247209.png" alt="космический корабль" />
     </div>
