56b30adea447be8a697451b7fa5376909f434544
Nikolai <pekarskiy.nikolai@gmail.com>
2019-07-11 10:46:57 +0300

    lesson 21-23 complete. add route exact. add css style and refactoring page dialogs

--- src/components/Dialogs/Dialogs.jsx
+++ src/components/Dialogs/Dialogs.jsx
@@ -1,10 +1,38 @@
 import React from 'react';
 import css from './Dialogs.module.css'; 
+import { NavLink } from 'react-router-dom';
+
+const DialogItem = (props) => {
+    return (
+        <div className={css.dialog + ' ' + css.active}>
+            <NavLink to={'/dialogs/' + props.id}>{props.name}</NavLink> 
+        </div>
+    )
+}
+
+const Message = (props) => {
+    return (
+        <div className={css.message}>{props.message}</div>
+    )
+}
 
 const Dialogs = (props) => {
     return (
         <div className={css.dialogs_box}>
-            Dialogs
+            <div className={css.dialogs_items}>
+                <DialogItem name='Dimych' id='1' />
+                <DialogItem name='Andrey' id='2' />
+                <DialogItem name='Sveta' id='3'  />
+                <DialogItem name='Sasha' id='4'  />
+                <DialogItem name='Viktor' id='5' />
+                <DialogItem name='Valera' id='6' />
+
+            </div>
+            <div className={css.messages}>
+                <Message message='Hi' />
+                <Message message='How is your it-kamasutra?' />
+                <Message message='Yo' />
+            </div>
         </div>
     )
 }
